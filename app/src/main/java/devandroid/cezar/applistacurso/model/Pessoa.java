f03dd88054bc5b7978a271988a7b02ddbdcf8366
CezarNegreiros <cezarnegreiros07@gmail.com>
2023-03-06 22:51:29 -0400

    Método toString - POO Herança e Polimorfismo

--- app/src/main/java/devandroid/cezar/applistacurso/model/Pessoa.java
+++ app/src/main/java/devandroid/cezar/applistacurso/model/Pessoa.java
@@ -3,7 +3,58 @@ package devandroid.cezar.applistacurso.model;
 public class Pessoa {
 
     // Construtor
+    // Atributos - Objetos - Molde - Modelo - Template
+    //Métodos de acesso - Getts and Setts
+
+    private String primeiroNome;
+    private String sobrenome;
+    private String numeroContato;
+    private String cursoDesejado;
 
     public Pessoa(){
+    }
+
+    public String getPrimeiroNome(){
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome(){
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNumeroContato(){
+        return numeroContato;
+    }
+
+    public void setNumeroContato(String numeroContato){
+        this.numeroContato = numeroContato;
+    }
+
+    public String getCursoDesejado(){
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado){
+        this.cursoDesejado = cursoDesejado;
+    }
+
+
+    //To String
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "primeiroNome='" + primeiroNome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", numeroContato='" + numeroContato + '\'' +
+                ", cursoDesejado='" + cursoDesejado + '\'' +
+                '}';
     }
 }
--- app/src/main/java/devandroid/cezar/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/cezar/applistacurso/view/MainActivity.java
@@ -10,11 +10,43 @@ import devandroid.cezar.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa, dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pessoa = new Pessoa();
+
+        //Atribuir conteúdo, dados, valores para o objeto
+        //Conforme o seu modelo
+        pessoa.setPrimeiroNome("Cezar");
+        pessoa.setSobrenome("Negreiros");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setNumeroContato("(92) 99123-4567");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Carlos");
+        outraPessoa.setSobrenome("Costa");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setNumeroContato("(92) 99345-6789");
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobrenome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Numero de contato: ";
+        dadosOutraPessoa += outraPessoa.getNumeroContato();
+
+
+
+        int parada = 0;
+
+        //dadosOutraPessoa = outraPessoa.getPrimeiroNome();
+
     }
 }
