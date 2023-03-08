4e187b10ac082d256dc794c3663ddf69ecfa2e9d
CezarNegreiros <cezarnegreiros07@gmail.com>
2023-03-08 12:19:19 -0400

    Criação SharedPreferences

--- app/src/main/java/devandroid/cezar/applistacurso/controller/PessoaController.java
+++ app/src/main/java/devandroid/cezar/applistacurso/controller/PessoaController.java
@@ -1,5 +1,24 @@
 package devandroid.cezar.applistacurso.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.cezar.applistacurso.model.Pessoa;
+
 public class PessoaController {
 
+    @NonNull
+    @Override
+    public String toString() {
+
+        Log.d("MVC_Controller", "Controller iniciado ");
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa){
+        Log.d("MVC_Controller", "Salvo: " + pessoa.toString());
+
+
+    }
 }
